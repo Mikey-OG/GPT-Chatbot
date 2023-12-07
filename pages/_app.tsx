13935d8745bc1f375846ba95d3ccb7666fb628a7
Mikey-OG <108473418+Mikey-OG@users.noreply.github.com>
2023-12-07 20:16:16 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
+
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
